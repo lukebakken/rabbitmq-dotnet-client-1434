1ebccb90030d57ae2fe986a1d69dd2aca9a1d78f
Luke Bakken <luke@bakken.io>
2023-12-04 08:17:58 -0800

    Add code provided by @pabermod

---
+++ RabbitMQRedirectAssembly/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace RabbitMQRedirectAssembly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+                ClientProvidedName = "MyClient",
+                Port = 5672,
+                AutomaticRecoveryEnabled = false,
+                TopologyRecoveryEnabled = false,
+                RequestedHeartbeat = TimeSpan.FromSeconds(10),
+                RequestedChannelMax = 10000,
+            };
+
+
+            var connection = factory.CreateConnection();
+
+            Console.WriteLine("CONNECTED");
+            Console.ReadKey();
+        }
+    }
+}
